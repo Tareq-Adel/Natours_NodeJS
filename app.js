4aa2345dfed75839969a38f318c066e4c19fc0e8
Tareq-Adel <tareq212adel@gmail.com>
2023-07-19 20:06:59 +0300

    Sanitize input user data

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
@@ -34,6 +36,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
 
 // Serving static file
 app.use(express.static(`./public`));
